18094420da6447bb196c00cc73b7947c35d08314
Ronny Herzog <r.herzog@entwicklerheld.de>
2022-12-07 20:47:00 +0100

    finished day 05

--- 05/index.js
+++ 05/index.js
@@ -3,7 +3,7 @@ const {movement} = require('./input.js');
 
 const instructions = movement.split('\n');
 
-function executeInstructions (instructions, startCrates) {
+function executeInstructions(instructions, startCrates, part) {
     let finalStacks = startCrates
     for (const instruction of instructions) {
         const instructionSplit = instruction.split(' ');
@@ -11,45 +11,41 @@ function executeInstructions (instructions, startCrates) {
         const from = parseInt(instructionSplit[3]);
         const to = parseInt(instructionSplit[5]);
 
-        finalStacks = moveCrates(finalStacks, amount, from, to);
+        if (part === 1) {
+            finalStacks = moveCratesWithCrateMover9000(finalStacks, amount, from, to);
+        }
+
+        if (part === 2) {
+            finalStacks = moveCratesWithCrateMover9001(finalStacks, amount, from, to);
+        }
     }
     return finalStacks
 }
 
-function moveCrates (crateStacks, amount, from, to) {
+function moveCratesWithCrateMover9000(crateStacks, amount, from, to) {
     for (let i = 0; i < amount; i++) {
-        const crate = crateStacks[from - 1].pop();
+        const crate = crateStacks[from - 1].shift();
         crateStacks[to - 1].unshift(crate);
     }
     return crateStacks
 }
 
+function moveCratesWithCrateMover9001(crateStacks, amount, from, to) {
+    const movedCrates = crateStacks[from - 1].slice(0, amount)
+    crateStacks[from - 1].splice(0, amount)
+    crateStacks[to - 1].splice(0, 0, ...movedCrates)
+    return crateStacks
+}
+
 function getTopCrates(crateStacks) {
     let result = ''
-    for(const crateStack of crateStacks) {
+    for (const crateStack of crateStacks) {
         result += crateStack[0]
     }
     return result
 }
 
-const result = executeInstructions(instructions, crates)
-
+const result = executeInstructions(instructions, crates, 2)
 
 console.log(result);
 console.log(getTopCrates(result));
-
-const testCrates = [
-  ['D', 'N', 'Z'],
-  ['C', 'M'],
-  ['P']
-]
-
-const testInstuctions = `move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2`
-
-const ti = testInstuctions.split('\n')
-
-
-console.log(executeInstructions(ti, testCrates));
