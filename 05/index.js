cbbcea5a76bfb51ce6e5e445aef33590a51108ab
rherzog <rherzog.dev@gmail.com>
2022-12-07 12:55:01 +0100

    wip day 05

--- 05/index.js
+++ 05/index.js
@@ -3,22 +3,53 @@ const {movement} = require('./input.js');
 
 const instructions = movement.split('\n');
 
-function executeInstructions(instructions) {
+function executeInstructions (instructions, startCrates) {
+    let finalStacks = startCrates
     for (const instruction of instructions) {
-        const instructionSplit = instruction.split(' ')
-        const amount = parseInt(instructionSplit[1])
-        const from = parseInt(instructionSplit[3])
-        const to = parseInt(instructionSplit[5])
+        const instructionSplit = instruction.split(' ');
+        const amount = parseInt(instructionSplit[1]);
+        const from = parseInt(instructionSplit[3]);
+        const to = parseInt(instructionSplit[5]);
 
-        moveCrates(amount, from, to)
+        finalStacks = moveCrates(finalStacks, amount, from, to);
     }
+    return finalStacks
 }
 
-function moveCrates (amount, from, to) {
-
-    crates = crates.splice(from + 1, amount)
+function moveCrates (crateStacks, amount, from, to) {
+    for (let i = 0; i < amount; i++) {
+        const crate = crateStacks[from - 1].pop();
+        crateStacks[to - 1].unshift(crate);
+    }
+    return crateStacks
 }
 
+function getTopCrates(crateStacks) {
+    let result = ''
+    for(const crateStack of crateStacks) {
+        result += crateStack[0]
+    }
+    return result
+}
+
+const result = executeInstructions(instructions, crates)
 
 
+console.log(result);
+console.log(getTopCrates(result));
 
+const testCrates = [
+  ['D', 'N', 'Z'],
+  ['C', 'M'],
+  ['P']
+]
+
+const testInstuctions = `move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+
+const ti = testInstuctions.split('\n')
+
+
+console.log(executeInstructions(ti, testCrates));
